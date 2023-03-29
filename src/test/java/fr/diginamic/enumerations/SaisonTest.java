0847cff074e692c1b4724fa1bb90ed67dde623f2
Newborshian <christopherbouthemy2014@gmail.com>
2023-03-29 18:35:34 +0200

    feat() [#1] Exercice 1 terminé

--- src/test/java/fr/diginamic/enumerations/SaisonTest.java
+++ src/test/java/fr/diginamic/enumerations/SaisonTest.java
@@ -9,12 +9,20 @@ class SaisonTest {
     @Test
     void valueOfLibelle() {
 
-        // Test valide
-        Saison saison1 = Saison.valueOfLibelle("Automne");
-        assertEquals(Saison.AUTOMNE, saison1);
+        //Test printemps
+        Saison printemps = Saison.valueOfLibelle("Printemps");
+        assertEquals(Saison.PRINTEMPS, printemps);
 
-        // Test valide deuxième essai
-        Saison saison = Saison.valueOfLibelle("Hiver");
-        assertEquals(Saison.HIVER, saison);
+        // Test Ete
+        Saison été = Saison.valueOfLibelle("Eté");
+        assertEquals(Saison.ETE, été);
+
+        // Test Automne
+        Saison automne = Saison.valueOfLibelle("Automne");
+        assertEquals(Saison.AUTOMNE, automne);
+
+        // Test Hiver
+        Saison hiver = Saison.valueOfLibelle("Hiver");
+        assertEquals(Saison.HIVER, hiver);
     }
 }
