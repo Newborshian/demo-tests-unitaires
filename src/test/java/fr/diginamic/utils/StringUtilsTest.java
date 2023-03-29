b3e84244c0e21ed9bc4d752769a5a4ba48e62ca2
Newborshian <christopherbouthemy2014@gmail.com>
2023-03-29 18:58:34 +0200

    feat() [#2] Exercice 2 terminé

--- src/test/java/fr/diginamic/utils/StringUtilsTest.java
+++ src/test/java/fr/diginamic/utils/StringUtilsTest.java
@@ -10,14 +10,18 @@ class StringUtilsTest {
     void levenshteinDistance() {
 
         // Test de chaînes de caractères identiques
-
+        assertEquals(2, StringUtils.levenshteinDistance("un" , "un"));
         // Test de chaînes de caractères différentes
-
+        assertEquals(1, StringUtils.levenshteinDistance("deux","troisième"));
         // Test de chaînes de caractères avec des espaces
-
+        assertEquals(0, StringUtils.levenshteinDistance(" ", ""));
         // Test de chaînes de caractères avec des accents
-
+        assertEquals(2, StringUtils.levenshteinDistance("été", "hiver"));
         // Test de chaînes de caractères avec des majuscules/minuscules
-
+        assertEquals(0, StringUtils.levenshteinDistance("Ete", "Hiver"));
+        // Operation mathématique + mixe string et nombre
+        assertEquals(0, StringUtils.levenshteinDistance("Ete+5", "X+4"));
+        // Paramètre null, cette boucle passe. Comment rendre plus robuste l'app ???
+        assertEquals(0, StringUtils.levenshteinDistance(null, null));
     }
 }
