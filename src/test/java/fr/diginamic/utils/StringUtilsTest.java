517f2fe9b4e75a96e9504b1a78e49fa8ac498794
Newborshian <christopherbouthemy2014@gmail.com>
2023-03-29 18:21:14 +0200

    feat() [#1] Exercice 1 terminé

---
+++ src/test/java/fr/diginamic/utils/StringUtilsTest.java
@@ -0,0 +1,23 @@
+package fr.diginamic.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringUtilsTest {
+
+    @Test
+    void levenshteinDistance() {
+
+        // Test de chaînes de caractères identiques
+
+        // Test de chaînes de caractères différentes
+
+        // Test de chaînes de caractères avec des espaces
+
+        // Test de chaînes de caractères avec des accents
+
+        // Test de chaînes de caractères avec des majuscules/minuscules
+
+    }
+}
